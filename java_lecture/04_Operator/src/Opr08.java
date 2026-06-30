1a8309022de3011f19e601553fca96751e0e2a52
Soon <soonmin.kim34@gmail.com>
2026-06-30 16:38:20 +0900

    Add Java Operator explanation comment on day 20 afternoon class

--- java_lecture/04_Operator/src/Opr08.java
+++ java_lecture/04_Operator/src/Opr08.java
@@ -10,7 +10,15 @@ public class Opr08 {
 		
 		System.out.println( "b++: " + b++ );
 		System.out.println( "b: " + b );
-		
+
+		/*
+			연산 순서
+			1. b변수에 10을 저장
+			2. a변수에 b변수에 저장된 10을 다시 저장
+			결론 : a, b변수에 저장된 값은 모두 10이 됨 
+			
+			a = 10,  b = 10
+		*/
 		a = b = 10 ;
 		
 		int c;
