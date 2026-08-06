20a8e2988b390984ab548df12cb4b3510330fe7d
Soon <soonmin.kim34@gmail.com>
2026-08-06 17:50:22 +0900

    Add Java Collection Practice

---
+++ java_lecture/25_Collection_Generic/src/BoardListExample.java
@@ -0,0 +1,8 @@
+
+public class BoardListExample {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ java_lecture/25_Collection_Generic/src/LinkedListTest.java
@@ -0,0 +1,8 @@
+
+public class LinkedListTest {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
